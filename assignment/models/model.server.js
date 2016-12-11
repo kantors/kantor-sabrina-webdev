7ca9cd6be6ae3be7f83feae87121d1ae00e48c97
kantors <kantor.sa@husky.neu.edu>
2016-12-11 03:44:28 -0500

    changing for remote database

--- assignment/models/model.server.js
+++ assignment/models/model.server.js
@@ -3,7 +3,24 @@
  */
 module.exports = function(app) {
   var mongoose = require('mongoose');
- mongoose.connect('mongodb://localhost/web-app-maker');
+
+  // this is the default connection string for local database
+  var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+  // then check for one of the environment variables
+  // if it exists, then you override the connection string
+  if(process.env.MLAB_USERNAME) {
+    var username = process.env.MLAB_USERNAME;
+    var password = process.env.MLAB_PASSWORD;
+    connectionString = 'mongodb://'+
+    process.env.MLAB_USERNAME + ':' +
+    process.env.MLAB_PASSWORD +
+    '@ds129018.mlab.com:29018/webdevnu';
+  }
+
+
+
+  mongoose.connect(connectionString);
 
   var UserModel = require("./user/user.model.server")();
   var WebsiteModel =  require("./website/website.model.server")();
